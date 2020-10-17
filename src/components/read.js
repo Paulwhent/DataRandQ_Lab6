ea061f1b68f6a81c4ae67522704604ec00887c6e
Paulwhent <48323848+Paulwhent@users.noreply.github.com>
2020-10-17 19:30:15 +0100

    Lab3 commit

---
+++ src/components/read.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export class Read extends React.Component {
+
+    render() {
+        return (
+
+            <div>
+                <h1>This is the Read Component</h1>
+            </div>
+        );
+    }
+}
