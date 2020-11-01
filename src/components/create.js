33587f86061bbe9b0eb94865afdac4e2f00f6726
Paulwhent <48323848+Paulwhent@users.noreply.github.com>
2020-11-01 11:56:35 +0000

    Lab4 submission

--- src/components/create.js
+++ src/components/create.js
@@ -1,12 +1,91 @@
+
+
 //Create 'Create' component
 import React from 'react';//import React to enable use of React library for single page application
+import './create.css'; //import css styling 
 export class Create extends React.Component {
 
+    //constructor for create class
+    constructor() {
+
+        //invoke constructor of parent class React.Component
+        super();
+        // bind all input events to this instance of class
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+
+        //create instance of state and initialise blank local string variables values to take data 
+        this.state = {
+
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    //on new Title input event, method to update state
+    onChangeTitle(e) {
+        this.setState({
+            Title: e.target.value
+        });
+    }
+
+    //on new Year input event, method to update state
+    onChangeYear(e) {
+        this.setState({
+            Year: e.target.value
+        });
+    }
+
+    //on new Poster input event, method to update state
+    onChangePoster(e) {
+        this.setState({
+            Poster: e.target.value
+        });
+    }
+
+    //method for submission button
+    onSubmit(e) {
+        //ensure button cannot be triggered again whilst still executing
+        e.preventDefault();
+        //alert to confirm movie Title added to state
+        alert("Movie: " + this.state.Title + " " + "Year: " + this.state.Year + " " + "Poster: " + this.state.Poster);
+        console.log("Button clicked");
+        console.log("Movie: " + this.state.Title + "   " + "Year: " + this.state.Year + "   " + "Poster: " + this.state.Poster);
+    }
     render() {
         return (
+            //add form to div with submission button that invokes onSubmit method
+            //use styling from App 
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
 
-            <div>
-                <h1>This is the Create Component</h1>
+                    <div className='form-group'>{/*form-group styling*/}
+                        <label>Movie Title Input</label>
+                        {/*text input box, form-control styling, add value to state when (Title) input event triggered*/}
+                        <input type='text' placeholder="Input Title here:" background-color="red" className='form-control'
+                         value={this.state.Title} onChange={this.onChangeTitle}></input>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Movie Year Input</label>
+                        <input type='text' placeholder="Input Year here:" className='form-control' 
+                        value={this.state.Year} onChange={this.onChangeYear}></input>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Movie Poster Input</label>
+                        <textarea type='text' placeholder="Input Poster link here:" className='form-control' 
+                        value={this.state.Poster} onChange={this.onChangePoster}></textarea>
+                    </div>
+
+                    <div className='form-group'>
+                        {/*submit button definition for adding input data*/}
+                        <input type='submit' value='Submit New Movie' className='btn btn-primary'></input>
+                    </div>
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,40 +1,37 @@
 import React from 'react';//import React to enable use of React library for single page application
 import { Movies } from './movies';
+import axios from 'axios';//import axios package to enable http requests (promise implementation)
+
 export class Read extends React.Component {
 
-    //state - object to store data (movies object) for use by this component (read)
+    //state - object to get/store data (movies object) for use by this component (read)
     state = {
-        movies: [ //movie information in Java Script Object Notation (JSON) format (in an array) with hyper links to jpg image files
-            {
-                "Title": "Avengers: Infinity War",
-                "Year": "2018",
-                "imdbID": "tt4154756",
-                "Type": "Action/Sci-fi",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "Action/Adventure",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Charlie Wilson's War",
-                "Year": "2007",
-                "imdbID": "tt0472062",
-                "Type": "Drama",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-        ]
+        movies: [] //movie information from connection to external web API using componentDidMount lifecycle hook
     };
+                //use lifecycle hook function/method componentDidMount to signal component/sub-components have rendered correctly/make Api call
+                //called each time component is active
 
+                componentDidMount() {
+                    //to set up 'promise' for json data from external url         
+                    axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+                        //for 'fullfilled' state
+                        .then(
+                            //anonymous method to get data in form of response if 'promise' fullfilled and update 'state' by populating movies array
+                            (response) => {
+                                this.setState({ movies: response.data.Search })
+                            })
+                        //for 'rejected' state
+                        .catch(
+                            //anonymous method to indicate error encountered
+                            (error)=>{console.log(error)}
+                        );
+                    }
     render() {
         return (
             //jsx (javascript in XML) code (allows HTML code in React, then transpiled to js) - Movies component in read 
             //create movies object in component Movies using state object that contains movie data in movies object from above
             <div>
-                <h1 style = {{color:"red"}}>Featured Movies</h1>
+                <h1 style={{ color: "red" }}>Featured Movies</h1>
                 <Movies movies={this.state.movies}></Movies>
             </div>
         );
